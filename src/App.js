1141fe6ca617126b86aed013a7908524d074892d
Virsaviya <14098014+virsaviya@users.noreply.github.com>
2025-03-16 17:45:18 -0700

    Create basic counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+
+import Button from './Button';
+
 function App() {
-  return <div>Build stuff here</div>;
+  const [count, setCount] = React.useState(0);
+
+  const increment = () => setCount(count + 1);
+
+  return (
+    <div className='wrapper'>
+      {count}
+      <br />
+      <Button onClick={increment} label='increment' />
+    </div>
+  );
 }
 
 export default App;
